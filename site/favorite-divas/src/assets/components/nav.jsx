9b71d29a172d40775fb8ff82cf42d6df72c4ced7
unknown <vilelakamy18@gmail.com>
2025-11-03 00:03:13 -0300

    dando os retoques finais

--- site/favorite-divas/src/assets/components/nav.jsx
+++ site/favorite-divas/src/assets/components/nav.jsx
@@ -3,7 +3,7 @@ import './nav.css';
 
 function Navbar(){
     return(
-        <nav class="navbar">
+        <nav className="navbar">
             <a href="#about">ABOUT</a>
             <a href="https://instagram.com/kamivilela.s" target="_blank">CONTACT</a>
         </nav>
